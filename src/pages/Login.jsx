e046bf8cd193e400daa3828d246370806aa41c07
Uttom <uttom.teacher.gps2@gmail.com>
2024-06-05 12:25:17 +0600

    Some correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,9 +54,7 @@ const Login = () => {
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            You can login using your email and password or google account.
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -60,9 +60,7 @@ const Registration = () => {
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Register now!</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          You can register using your email and password or google account.
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
